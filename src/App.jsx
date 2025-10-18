e25b0ef8bbc7807cdc894e16cc714c2c2195253b
Sumaiiya-Haque <sumaiyahaque475@gmail.com>
2025-10-18 16:34:44 +0600

    header setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <h1 className='text-5xl text-red-500'>Let's Build Dragon News</h1>
+    
+    </>
+  )
+}
+
+export default App
